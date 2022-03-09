2191e803292b03a5d458ee8b541b62ab8dd4cf8a
Sindhu Naik <97089586+SindhunaikAbcd@users.noreply.github.com>
2022-03-09 04:22:29 +0000

    .....

--- p2final.c
+++ p2final.c
@@ -1,1 +1,27 @@
 #include<stdio.h>
+int input()
+{
+  int a ;
+  printf("Enter a number:\n");
+  scanf("%d", &a);
+  return a;
+}
+int sum(int a, int b)
+{
+  int c;
+  c=a+b;
+  return c;
+}
+void output(int a, int b, int c)
+{
+  printf("%d+%d=%d\n",a, b, c);
+}
+int main()
+{
+  int a, b, c;
+  a=input();
+  b=input();
+  c=sum(a,b);
+  output(a,b,c);
+  return 0;
+}
