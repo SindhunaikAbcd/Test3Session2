fdefd091782c9ccb4c726462d80bb753583e5c6d
Sindhu Naik <97089586+SindhunaikAbcd@users.noreply.github.com>
2022-03-09 04:41:39 +0000

    .......

--- p4final.c
+++ p4final.c
@@ -1,45 +1,34 @@
-/*
-
-Write a program to find nth number in fibonacci sequence.
-Fibonacci sequence consists of 0,1,1,2,3,5,8,13,21........
-int input();
-int find_fibo(int n);
-void output(int n, int fibo);
-
-*/
-
-#include <stdio.h>
-
+#include<stdio.h>
 int input()
 {
-  int n;
-  printf("Enter the number\n");
-  scanf("%d",&n);
-  return n;
+  int a;
+  printf("Enter a number\n");
+  scanf("%d", &a);
+  return a;
 }
-int find_fibo(int n)
+int cmp(int a, int b, int c)
 {
-  int fibo=0;
-  int a=0;
-  int b=1;
-  for(int i=0;i<n;i++) {
-    fibo = a;
-    a = b;
-    b = fibo + b;
-  }
-  return fibo;
+  if(a>b && a>c);
+    return a;
+  
+  if (b>c)
+   return b;
+  
+  else 
+   return c;
 }
-
-
-void output(int n, int fibo)
+void output(int a, int b,int c,int large)
 {
-  printf("the %dth fibonacci number is %d\n", n, fibo);
+  printf("%d is the greatest number\n",large);
 }
-
 int main()
 {
-  int n = input();
-  int fibo = find_fibo(n);
-  output(n, fibo);
+  int a, b, c, large;
+  a=input();
+  b=input();
+  c=input();
+  large=cmp(a, b, c);
+  output(a,b,c,large);
   return 0;
 }
+
